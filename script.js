b3187f975212bc6ea5bb3d66202408d1ab6aee0b
Serhii Tyshchenko <st@estem.de>
2024-08-12 12:55:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('🚫 No number !');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct number !');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').telue = '';
+});
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
